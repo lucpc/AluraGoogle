96eed5a763f3e4dbc3504da7ea5a899bac2b3358
Lucpc <lucpcaldas@gmail.com>
2024-09-06 09:07:04 -0300

    "informações sobre os atletas inseridas pelo js"

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+// dados.forEach(item =>{
+//     section.innerHTML = `
+// <div class = "item-resultado"> 
+//     <h2>
+//         <a href = "https://www.instagram.com/rebecarandrade/?hl=pt-br" target="_blank">${dados[item].titulo}/a>
+//     </h2>
+//     <p class = "descricao-meta">${dados[item].descricao}</p>
+//     <a href="${dados[item].link}" target="_blank"> Mais informações</a>
+// </div>
+// `
+// });
+let resultados = "";
+for (let dado of dados){
+    resultados += `
+<div class = "item-resultado"> 
+    <h2>
+        <a href = "https://www.instagram.com/rebecarandrade/?hl=pt-br" target="_blank">${dados[item].titulo}/a>
+    </h2>
+    <p class = "descricao-meta">${dados[item].descricao}</p>
+    <a href="${dados[item].link}" target="_blank"> Mais informações</a>
+</div>
+`
+}
+
+section.innerHTML = resultados;
